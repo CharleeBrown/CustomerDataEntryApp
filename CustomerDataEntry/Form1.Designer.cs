a9e427cc28e5c43274c710b81f4684fec962567d
coleman hallom <challom@stu.clcillinois.edu>
2020-03-14 15:13:55 -0500

    Add project files.

---
+++ CustomerDataEntry/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace CustomerDataEntry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.phoneNum = new System.Windows.Forms.TextBox();
+            this.companyName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(38, 35);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(92, 20);
+            this.firstName.TabIndex = 0;
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(156, 36);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(96, 20);
+            this.lastName.TabIndex = 1;
+            // 
+            // phoneNum
+            // 
+            this.phoneNum.Location = new System.Drawing.Point(292, 36);
+            this.phoneNum.Name = "phoneNum";
+            this.phoneNum.Size = new System.Drawing.Size(93, 20);
+            this.phoneNum.TabIndex = 2;
+            this.phoneNum.TextChanged += new System.EventHandler(this.phoneNum_TextChanged);
+            // 
+            // companyName
+            // 
+            this.companyName.Location = new System.Drawing.Point(415, 36);
+            this.companyName.Name = "companyName";
+            this.companyName.Size = new System.Drawing.Size(112, 20);
+            this.companyName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(430, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Company Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Submit ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 136);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.companyName);
+            this.Controls.Add(this.phoneNum);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
+            this.Name = "Form1";
+            this.Text = "Customer Data Entry";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.TextBox phoneNum;
+        private System.Windows.Forms.TextBox companyName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CustomerDataEntry/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CustomerDataEntry
+{
+    public partial class Form1 : Form
+    {
+
+        Connector connector = new Connector();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void phoneNum_TextChanged(object sender, EventArgs e)
+        {
+            if(phoneNum.TextLength == 3)
+            {
+                phoneNum.Text = phoneNum.Text + "-";
+                phoneNum.SelectionStart = phoneNum.TextLength;
+            }
+            if (phoneNum.TextLength == 7)
+            {
+                phoneNum.Text = phoneNum.Text + "-";
+                phoneNum.SelectionStart = phoneNum.TextLength;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(firstName.Text)||string.IsNullOrEmpty(lastName.Text)||string.IsNullOrEmpty(phoneNum.Text)||
+
+                string.IsNullOrEmpty(companyName.Text))
+                {
+                 MessageBox.Show("Enter missing information", "Customer Entry", MessageBoxButtons.OK);
+                }
+            else {
+                connector.SubmitData(firstName.Text, lastName.Text, phoneNum.Text, companyName.Text);
+                firstName.Text = " ";
+                lastName.Text = " ";
+                phoneNum.Text = "";
+                companyName.Text = " ";
+                firstName.Focus();
+            }
+            
+        }
+        
+    }
+    public class Connector
+
+    {
+        
+
+        public  void SubmitData(string fName, string lastname, string phoneNum, string comp)
+        {
+            
+            SqlConnection mainConn = new SqlConnection();
+        
+            mainConn.ConnectionString = ConfigurationManager.ConnectionStrings["maindb"].ConnectionString;
+
+            string enterData = @"INSERT INTO customerInfo  VALUES ('"+fName+"' ,'"+ lastname + "' ,'"+phoneNum+"','"+comp+"');";
+
+            SqlCommand dataComm = new SqlCommand(enterData);
+
+            dataComm.Connection = mainConn;
+
+            using (mainConn)
+            {
+                try
+                {
+                    mainConn.Open();
+                    dataComm.ExecuteNonQuery();
+
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    mainConn.Close();
+                    
+                }
+                
+                
+            }
+            
+            
+        }
+
+    }
+}
